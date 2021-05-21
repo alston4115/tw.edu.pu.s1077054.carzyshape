8ac17a471564f541daf9facd8ea34b1e32b83b3e
alston4115 <alston4115@gmail.com>
2021-05-21 13:42:03 +0800

    2021上機考v1_陳煜棠

--- app/src/main/java/com/example/twedupus1077054carzyshape/MainActivity.kt
+++ app/src/main/java/com/example/twedupus1077054carzyshape/MainActivity.kt
@@ -2,13 +2,24 @@ package com.example.twedupus1077054carzyshape
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ImageView
+import com.bumptech.glide.annotation.GlideModule
+import com.bumptech.glide.module.AppGlideModule
+
+@GlideModule
+public final class MyAppGlideModule : AppGlideModule()
+
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        //setContentView(R.layout.activity_main)
-        setContentView(GameActivity(this))
+        setContentView(R.layout.activity_main)
 
+        val img:ImageView = findViewById(R.id.imgTitle)
+        GlideApp.with(this)
+            .load(R.drawable.cover)
+            .override(800,600)
+            .into(img)
 
 
     }
